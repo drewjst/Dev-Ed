4b6c90ec7c43d77abcea52b129cee3f3618b34ad
Andrew <drew.jst@gmail.com>
2017-07-19 09:19:47 -0700

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -27,3 +27,16 @@ def bigger(a,b):
 # Find the biggest value of three inputs
 def biggest(a, b, c):
     return bigger(bigger(a,b), c)
+
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints 
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+def print_numbers(n):
+    i = 1
+    while i < n:
+        print(i)
+        i = i + 1
